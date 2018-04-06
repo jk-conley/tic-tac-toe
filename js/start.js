b06841bd88679e9c82d9eeee3e5f6be1bd10b3c0
James_K_Conley <jkconleydec2012@gmail.com>
2018-04-06 15:42:30 -0700

    Refactored the create page to use modern string interpolation over concat

--- js/start.js
+++ js/start.js
@@ -10,12 +10,13 @@
     $('#board').hide();
 
     // create start page
-    $('body').append('<div class="screen screen-start" id="start">' +
-      '<header>' +
-      '<h1>Tic Tac Toe</h1>' +
-      '<a href="#" class="button">Start game</a>' +
-      '</header>' +
-      '</div>');
+    $('body').append(`
+      <div class="screen screen-start" id="start">
+        <header>
+          <h1>Tic Tac Toe</h1>
+          <a href="#" class="button">Start game</a>
+        </header>
+      </div>`);
 
     // on page load show start page
     $('#start').show();
