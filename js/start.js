9c23113ce10e42bb2a2be35e2aa4e12bd5d41511
James_K_Conley <jkconleydec2012@gmail.com>
2018-04-07 16:22:04 -0700

    Added Player Name functionality

--- js/start.js
+++ js/start.js
@@ -22,6 +22,13 @@ const startGame = () => {
   // when player clicks "Start game"
   $('#start .button').on("click", () => {
 
+    // get player name
+    const name = prompt("What is your name?");
+    console.log(name);
+
+    // display player name
+    $('#board header').append(`<p class="player-name">Player: ${name}</p>`)
+
     // hide start page
     $('#start').hide();
 
